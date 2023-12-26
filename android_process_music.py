bfc2eb7739ed8910cdb5d1179af43f1c8f705dd6
swablueme <swablueme@gmail.com>
2023-12-26 13:16:42 +1100

    update to suport opus and m4a sorting

--- android_process_music.py
+++ android_process_music.py
@@ -1,18 +1,25 @@
 import eyed3
-import os, shutil
+import os
+import shutil
 import mutagen
+from mutagen.oggopus import OggOpus
 from mutagen.mp4 import MP4
 from mutagen.id3 import APIC, ID3
 import re
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
 eyed3.log.setLevel("ERROR")
 print("Current working directory: {0}".format(os.getcwd()))
-music_folder="to process"
-to_output_to="output"
+music_folder = "to process"
+to_output_to = "output"
+
+
 class process:
     def __init__(self):
         if not os.path.exists(music_folder):
             os.mkdir(music_folder)
-        self.music_list=self.load_mp3s()
+        self.music_list = self.load_mp3s()
         print("loading", self.music_list)
         self.remove_artwork()
 
@@ -25,21 +32,25 @@ class process:
                 mp3 = ID3(audio)
                 mp3.delall("APIC")
                 mp3.save()
-                process.sort_folders(mp3, audio, mp3['TPE1'].text[0])
+                artist_name = mp3['TPE1'].text[0]
+                if artist_name is None:
+                    artist_name = mp3.tag.artist
+                process.sort_folders(audio, artist_name)
             elif audio.endswith(".m4a"):
-                process.sort_folders(mp3, audio, MP4(audio).tags["©ART"][0])
-            
+                process.sort_folders(audio, MP4(audio).tags["©ART"][0])
+            elif audio.endswith(".opus"):
+                process.sort_folders(audio, OggOpus(audio).tags["artist"][0])
+
     @staticmethod
-    def sort_folders(mp3, audio_location, artist_name=None):
-        if artist_name is None:
-            artist_name=mp3.tag.artist
-        #artist_name=artist_name.replace(":"," ")
-        artist_name=re.sub("[:|\\|\/|\*|\"|<|>|\|]", "",artist_name)
+    def sort_folders(audio_location, artist_name=None):
+        # artist_name=artist_name.replace(":"," ")
+        artist_name = re.sub("[:|\\|\/|\*|\"|<|>|\|]", "", artist_name)
         print(artist_name)
-        where_to_output=os.path.join(to_output_to, artist_name)
+        where_to_output = os.path.join(to_output_to, artist_name)
         if not os.path.exists(where_to_output):
             os.makedirs(where_to_output)
-        shutil.move(audio_location, os.path.join(where_to_output, os.path.basename(audio_location)))
-        
-        
+        shutil.move(audio_location, os.path.join(
+            where_to_output, os.path.basename(audio_location)))
+
+
 process()
